22109e5577d8fcbc4b2008a9a6e9f4af6731ed2f
coristig <colin.ristig@gmail.com>
2017-09-21 18:21:34 -0400

    added iris example model

---
+++ examples/iris-classifier/train.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+
+# Create and fit a nearest-neighbor classifier
+knn = KNeighborsClassifier()
+knn.fit(X, y) 
+KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',
+           metric_params=None, n_jobs=1, n_neighbors=5, p=2,
+           weights='uniform')
+
+# save our trained model to a pickle file so we can use it later
+from sklearn.externals import joblib
+joblib.dump(knn, './objects/model_weights.pkl')
+
