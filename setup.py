eed5aa8490927d5822a6c1bbceab152afc41dc73
Greg Lamp <lamp.greg@gmail.com>
2017-09-21 11:58:28 -0400

    reqs

--- setup.py
+++ setup.py
@@ -19,9 +19,6 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'promote', 'v
         classifiers=(
         ),
         install_requires=[
-            "pip",
-            # "dill==0.2.5",
-            # "terragon==0.3.1",
             "progressbar2==3.10.1",
             "requests==2.11.1",
             "requests-toolbelt==0.7.0"
