74267fa5ffcaeeb31079fecae07498340ded9b95
David Cooperberg <dcooperberg@alteryx.com>
2018-04-18 15:43:44 -0600

    save h2o model logloss as model metadata

--- examples/h2o-classifier/deploy.py
+++ examples/h2o-classifier/deploy.py
@@ -4,7 +4,7 @@ import os
 
 h2o.init()
 
-model_path = os.path.abspath('./objects/DRF_model_python_1512530312673_3')
+model_path = os.path.abspath('./objects/DRF_model_python_1524085511585_1')
 model = h2o.load_model(model_path)
 
 def helloh2o(data):
@@ -20,6 +20,9 @@ PROMOTE_URL = "https://promote.c.yhat.com/"
 
 
 p = promote.Promote(USERNAME, API_KEY, PROMOTE_URL)
+
+# add metadata
+p.metadata.logloss = float("{0:.5f}".format(model.logloss()))
 
 # test data
 TESTDATA = {"C1":4.9,"C2":3,"C3":1.4,"C4":0.2}
