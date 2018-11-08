c829ba7e434b6e373e1d62578226f7777e7fad87
David Cooperberg <dcooperberg@alteryx.com>
2018-11-08 11:59:14 -0700

    removed updates to h20 that dont work

--- examples/h2o-classifier/promote.sh
+++
@@ -1,5 +0,0 @@
-add-apt-repository -y ppa:webupd8team/java
-apt-get update
-echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
-apt-get -y install oracle-java8-installer 
